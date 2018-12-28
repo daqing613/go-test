fd267837613029568da86b6e631d2a083ed43254
daniel wang <xoxo1987@live.cn>
2018-12-28 14:00:44 +0800

    Go json with struct
    Signed-off-by: daniel wang <xoxo1987@live.cn>

--- go-json/go-json.go
+++ go-json/go-json.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
 )
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+type User struct {
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Age    int    `json:"age"`
+	Social Social `json:"social"`
+}
+
+type Social struct {
+	Facebook string `json:"facebook"`
+	Twitter  string `json:"twitter"`
+}
 
 func main() {
 
@@ -17,4 +36,18 @@ func main() {
 	fmt.Println("Successfully Opened user.json")
 
 	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var users Users
+
+	json.Unmarshal(byteValue, &users)
+
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Type: " + users.Users[i].Type)
+		fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+	}
+
 }
